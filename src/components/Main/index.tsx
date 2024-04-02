114fc8ce9ef53cfead2719d35c370a86237a14e3
이운린 <105215297+likegitman@users.noreply.github.com>
2024-04-02 13:47:56 +0900

    Add text animation

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,23 @@
 import Video from '@/components/Common/Video'
+import './style.css'
 
 const MainVideo = () => {
   return (
     <section
       id='home'
-      className='flex items-center justify-center w-full px-16'
+      className='flex flex-col items-center justify-center w-full px-16 gap-6'
       style={{ height: 'calc(100vh - 5rem)' }}
     >
-      <div className='w-full max-w-6xl aspect-video'>
+      <div className='flow'>
+        <div className='flex items-center gap-6'>
+          <img src='/svg/sewol-ribbon.svg' />
+          <p className='text-[#F2CD3A] text-2xl font-semibold'>
+            세월호 10주기입니다, 그날의 아픔과 상처를 잊지않고 기억하겠습니다.
+          </p>
+          <img src='/svg/sewol-ribbon.svg' />
+        </div>
+      </div>
+      <div className='w-full max-w-60xl aspect-video'>
         <Video id='j5sxdl5_hPc' />
       </div>
     </section>
